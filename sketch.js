16e81809bf198ae75a8baef01980060458425c35
ZERROZ28 <kylian.bourdelas28@gmail.com>
2025-12-17 11:02:35 +0100

    Update

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 let inputText, fontSize, colorText, colorBG;
-let textX, textY, lineSpacing;
+let textX, textY;
 let weight, tracking, blurSlider, scaleYInput, fontSelect, spreadSlider;
 
 let scaleYValue = 1;
 let selectedFont = "Roboto Flex";
 let brushActive = false;
 
-let letters = []; // lettres du texte principal
+let letters = [];
 
 function setup() {
   let preview = select("#previewContainer");
@@ -20,7 +20,6 @@ function setup() {
   colorBG = select("#colorBG");
   textX = select("#textX");
   textY = select("#textY");
-  lineSpacing = select("#lineSpacing");
   weight = select("#weight");
   tracking = select("#tracking");
   blurSlider = select("#blur");
@@ -30,105 +29,105 @@ function setup() {
 
   textFont(selectedFont);
 
-  // Listeners : redessiner le texte à chaque changement
+  // Listeners
   inputText.input(() => { rebuildLetters(); drawMainText(); });
   fontSize.input(() => { rebuildLetters(); drawMainText(); });
   tracking.input(() => { rebuildLetters(); drawMainText(); });
   weight.input(() => { rebuildLetters(); drawMainText(); });
-  lineSpacing.input(() => { rebuildLetters(); drawMainText(); });
   spreadSlider.input(() => { rebuildLetters(); drawMainText(); });
-  scaleYInput.input(() => { scaleYValue = Number(scaleYInput.value()); drawMainText(); });
-  fontSelect.input(() => { selectedFont = fontSelect.value(); textFont(selectedFont); drawMainText(); });
+  scaleYInput.input(() => {
+    scaleYValue = Number(scaleYInput.value());
+    drawMainText();
+  });
+  fontSelect.input(() => {
+    selectedFont = fontSelect.value();
+    textFont(selectedFont);
+    rebuildLetters();
+    drawMainText();
+  });
   textX.input(() => { rebuildLetters(); drawMainText(); });
   textY.input(() => { rebuildLetters(); drawMainText(); });
 
-  // Brush Button
+  // Brush toggle
   let toggleBrushBtn = select("#toggleBrush");
-  if(toggleBrushBtn){
+  if (toggleBrushBtn) {
     toggleBrushBtn.mousePressed(() => {
       brushActive = !brushActive;
       toggleBrushBtn.html(brushActive ? "Brush Activé" : "Activer Brush");
     });
   }
 
-  // ✅ AJOUT : Boutons de sauvegarde
+  // Save buttons
   let savePNGBtn = select("#savePNG");
   let saveJPGBtn = select("#saveJPG");
 
   if (savePNGBtn) {
-    savePNGBtn.mousePressed(() => {
-      saveCanvas('TextERA_export', 'png');
-    });
+    savePNGBtn.mousePressed(() => saveCanvas("TextERA_export", "png"));
   }
 
   if (saveJPGBtn) {
-    saveJPGBtn.mousePressed(() => {
-      saveCanvas('TextERA_export', 'jpg');
-    });
+    saveJPGBtn.mousePressed(() => saveCanvas("TextERA_export", "jpg"));
   }
 
   rebuildLetters();
-  drawMainText(); // dessine le texte initial
+  drawMainText();
 }
 
-// Construit la position des lettres avec éclatement aléatoire
+// =======================================================
+// BUILD LETTERS (SINGLE LINE ONLY)
+// =======================================================
 function rebuildLetters() {
   letters = [];
-  let txt = inputText.value();
+
+  let txt = inputText.value().replace(/\n/g, "");
+  if (txt.length === 0) return;
+
   let fs = Number(fontSize.value());
   let offsetX = Number(textX.value());
   let offsetY = Number(textY.value());
-  let spacing = fs * Number(lineSpacing.value());
   let boldFactor = map(Number(weight.value()), 100, 900, 1, 5);
   let track = Number(tracking.value());
   let spread = Number(spreadSlider.value() || 0);
 
-  let lines = txt.split("\n");
+  let charSpacing = fs * track + boldFactor * 1.8;
+  let totalWidth = (txt.length - 1) * charSpacing;
+  let baseX = -totalWidth / 2;
 
-  for (let i = 0; i < lines.length; i++) {
-    let line = lines[i];
-    let yLine = i * spacing - ((lines.length - 1) * spacing) / 2;
+  let xCursor = baseX;
 
-    let charSpacing = fs * track + boldFactor * 1.8;
-    let totalWidth = (line.length - 1) * charSpacing;
-    let baseX = -totalWidth / 2; // centré horizontalement
+  for (let i = 0; i < txt.length; i++) {
+    let sx = random(-spread, spread);
+    let sy = random(-spread, spread);
 
-    let xCursor = baseX;
+    letters.push({
+      char: txt[i],
+      x: width / 2 + offsetX + xCursor + sx,
+      y: height / 2 + offsetY + sy,
+    });
 
-    for (let j = 0; j < line.length; j++) {
-      let ch = line[j];
-
-      // Éclatement aléatoire
-      let sx = random(-spread, spread);
-      let sy = random(-spread, spread);
-
-      letters.push({
-        char: ch,
-        x: width / 2 + offsetX + xCursor + sx,
-        y: height / 2 + offsetY + yLine + sy,
-      });
-
-      xCursor += charSpacing;
-    }
+    xCursor += charSpacing;
   }
 }
 
-// Dessine le texte principal
+// =======================================================
+// DRAW MAIN TEXT
+// =======================================================
 function drawMainText() {
-  if(!colorBG) return;
+  if (!colorBG) return;
+
   background(color(colorBG.value()));
 
   let fs = Number(fontSize.value());
   let blurVal = Number(blurSlider.value());
 
-  for (let l of letters){
+  for (let l of letters) {
     push();
     textSize(fs);
     textStyle(BOLD);
     textAlign(CENTER, CENTER);
     fill(color(colorText.value()));
 
-    if(blurVal > 0){
+    if (blurVal > 0) {
       drawingContext.shadowBlur = blurVal * 2;
       drawingContext.shadowColor = color(colorText.value()).toString();
     } else {
@@ -142,36 +141,40 @@ function drawMainText() {
   }
 }
 
-// Draw pour le brush animé autour de la souris
+// =======================================================
+// BRUSH MODE
+// =======================================================
 function draw() {
-  if(brushActive && frameCount % 2 === 0){
-    let fs = Number(fontSize.value());
-    let blurVal = Number(blurSlider.value());
+  if (!brushActive || letters.length === 0) return;
+  if (frameCount % 2 !== 0) return;
 
-    // Calcul centre du texte
-    let centerX = 0, centerY = 0;
-    for (let l of letters){
-      centerX += l.x;
-      centerY += l.y;
-    }
-    centerX /= letters.length;
-    centerY /= letters.length;
+  let fs = Number(fontSize.value());
+  let blurVal = Number(blurSlider.value());
 
-    for(let l of letters){
-      drawLetterBrush(l, fs, blurVal, centerX, centerY);
-    }
+  let centerX = 0;
+  let centerY = 0;
+
+  for (let l of letters) {
+    centerX += l.x;
+    centerY += l.y;
+  }
+
+  centerX /= letters.length;
+  centerY /= letters.length;
+
+  for (let l of letters) {
+    drawLetterBrush(l, fs, blurVal, centerX, centerY);
   }
 }
 
-function drawLetterBrush(l, fs, blurVal, centerX, centerY){
+function drawLetterBrush(l, fs, blurVal, centerX, centerY) {
   push();
   textSize(fs);
   textStyle(BOLD);
   textAlign(CENTER, CENTER);
-
   fill(color(colorText.value()));
 
-  if(blurVal > 0){
+  if (blurVal > 0) {
     drawingContext.shadowBlur = blurVal * 2;
     drawingContext.shadowColor = color(colorText.value()).toString();
   } else {
@@ -197,7 +200,10 @@ function drawLetterBrush(l, fs, blurVal, centerX, centerY){
   pop();
 }
 
-function windowResized(){
+// =======================================================
+// RESIZE
+// =======================================================
+function windowResized() {
   let preview = select("#previewContainer");
   resizeCanvas(preview.width || windowWidth, windowHeight);
   rebuildLetters();
